f93ca7a5477d4197455c8e288e6c0ccf6b9f4f88
Rohman Habib M <mhrohman@live.com>
2019-03-23 14:10:21 +0700

    feat(*): add missing dotdrop files

---
+++ dotfiles/config/fish/config.fish
@@ -0,0 +1,5 @@
+set -gx PATH $PATH ~/.npm-global/bin /usr/local/go/bin ~/Codes/lib/flutter/bin 
+set GOROOT ~/GO  
+set -Ux BUILD_ENV production
+alias dotdrop='~/dotfiles/dotdrop.sh --cfg=~/dotfiles/config.yaml'
+
